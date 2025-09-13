5fd9b5e82b3c3bdc36cd7c09cc56b8ca46aa3bc1
sumitbansode360 <sumitbansode360@gmail.com>
2025-09-13 20:27:52 +0530

    sidebar added

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -4,7 +4,8 @@ import { Button } from "@/components/ui/button"
 function page() {
   return (
     <div>      
-        <Button>Click me</Button>
+        {/* <Button>Click me</Button> */}
+        <p className='bg-blue-500 h-full'>Home</p>
     </div>
   )
 }
