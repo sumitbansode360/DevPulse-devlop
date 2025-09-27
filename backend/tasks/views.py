388892b95d56698830e6b7c92d330e726d109fe4
sumitbansode360 <sumitbansode360@gmail.com>
2025-09-27 14:43:10 +0530

    pagination added in task api

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -5,9 +5,16 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination 
+
+class TaskPagination(PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class TaskViewSet(ModelViewSet):
     serializer_class = TaskSerializer
+    pagination_class = TaskPagination
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['status']
     search_fields = ['title', 'description']
@@ -21,19 +28,23 @@ class TaskViewSet(ModelViewSet):
     
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            response = self.get_paginated_response(serializer.data)
+        else:
+            serializer = self.get_serializer(queryset, many=True)
+            # This part is for when pagination is disabled
+            response = Response({"results": serializer.data}) 
         
         # Counts for all tasks (not just filtered)
         all_count = Task.objects.filter(user=request.user).count()
         pending_count = Task.objects.filter(user=request.user, status='pending').count()
         completed_count = Task.objects.filter(user=request.user, status='completed').count()
 
-        return Response({
-            "results": serializer.data,
-            "count" : {
-                "all": all_count,
-                "pending": pending_count,
-                "completed": completed_count
-            
-            }
-        })
+        response.data['count'] = {
+            "all": all_count,
+            "pending": pending_count,
+            "completed": completed_count
+        }
+        return response
